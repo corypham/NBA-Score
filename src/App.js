8b872b27da62daea5478381660c12ff7c3078279
ritakura <reina.itk@gmail.com>
2023-01-03 22:42:43 -0800

    kind of works?

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+   // new line start
+  const [games, setGameScores] = useState(null)
+
+  function getData() {
+    axios({
+      method: "GET",
+      url:"http://localhost:3000/data",
+    })
+    .then((response) => {
+      const res = response.data
+      setGameScores(({
+        games: res.games,
+      }))
+      console.log(res);
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })}
+    //end of new line 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+        {games && <div>
+              <p>games: {games.games}</p>
+            </div>
+        }
       </header>
     </div>
   );

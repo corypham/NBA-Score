144d18206f3e1b7ca0750754950e8e3bc46532fc
ritakura <reina.itk@gmail.com>
2023-02-05 23:07:02 -0800

    update

--- src/App.js
+++ src/App.js
@@ -25,21 +25,20 @@ function App() {
   return (
     <div className="App">
       <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,800&family=Quicksand&display=swap" rel="stylesheet"></link>
-      <header className="App-header">
-        <div className="games">
-          <div className="dropdown">
-            <label for="gameType">Choose Game Type:</label>
-            <select className="gameType" id="gameType" value={game_type} onChange={handleChange}>
-              <option value="live">Live</option>
-              <option value="past">Past</option>
-              <option value="future">Future</option>
-            </select>
-          </div>
-          {game_type==='live' && <LiveData />}
-          {game_type==='past' && <PastGames />}
-          {game_type==='future' && <FutureMatchups />}
+      {/* <header className="App-header"> </header> */}
+      <div className="games">
+        <div className="dropdown">
+          <label for="gameType">Choose Game Type:</label>
+          <select className="gameType" id="gameType" value={game_type} onChange={handleChange}>
+            <option value="live">Live</option>
+            <option value="past">Past</option>
+            <option value="future">Future</option>
+          </select>
         </div>
-      </header>
+        {game_type==='live' && <LiveData />}
+        {game_type==='past' && <PastGames />}
+        {game_type==='future' && <FutureMatchups />}
+      </div>
     </div>
   );
 }

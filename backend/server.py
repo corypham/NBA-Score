6ebd02b20341045efb87ad39995a1bc74da61ec9
ritakura <reina.itk@gmail.com>
2023-02-11 07:35:42 -0800

    fix a lot of things and also implemented some more css stuff

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,15 @@
-# Import flask and datetime module for showing date and time
+# Imports
 from flask import Flask
 import json
-  
 import math
+from datetime import datetime, timezone, date, timedelta
+
+from nba_api.live.nba.endpoints import scoreboard
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import scoreboard as board_of_scores
 # Initializing flask app
 app = Flask(__name__)
 
-# flask
-
-from datetime import datetime, timezone, date, timedelta
 def get_date(num_days):
     if num_days == -1:
         return 'Yesterday'
@@ -22,27 +23,49 @@ def get_date(num_days):
         return day.strftime('%m/%d/%Y')
 
 # fetch live data
-from nba_api.live.nba.endpoints import scoreboard
 def fetch():
     board = scoreboard.ScoreBoard()
     print("ScoreBoardDate: " + board.score_board_date)
-    games = board.games.get_dict()
-    game_data = [{}] * len(games)
-    for i in range (len(games)):
-        game_data[i] = {
-            "gameStatus": (games[i])['gameStatusText'],
-            "awayTeamData": {
-                "logoURL": (games[i])['awayTeam']['teamTricode'],
-                "name": (games[i])['awayTeam']['teamName'],
-                "score": (games[i])['awayTeam']['score']
-                }, 
-            "homeTeamData": {
-                "logoURL": (games[i])['homeTeam']['teamTricode'],
-                "name": (games[i])['homeTeam']['teamName'],
-                "score": (games[i])['homeTeam']['score']
-                }, 
-            }
-        print("hello world", game_data[i]['awayTeamData']['logoURL'])
+    today = date.today()
+    if board.score_board_date == today:
+        games = board.games.get_dict()
+        game_data = [{}] * len(games)
+        for i in range (len(games)):
+            game_data[i] = {
+                "gameStatus": (games[i])['gameStatusText'],
+                "awayTeamData": {
+                    "logoURL": (games[i])['awayTeam']['teamTricode'],
+                    "name": (games[i])['awayTeam']['teamName'],
+                    "score": (games[i])['awayTeam']['score']
+                    }, 
+                "homeTeamData": {
+                    "logoURL": (games[i])['homeTeam']['teamTricode'],
+                    "name": (games[i])['homeTeam']['teamName'],
+                    "score": (games[i])['homeTeam']['score']
+                    }, 
+                }
+    else:
+        board = board_of_scores.Scoreboard()
+        live_games = board.get_normalized_dict()
+        pg = live_games['LineScore']
+        s = live_games['GameHeader']
+
+        game_data = [{}] * (math.ceil(len(pg)/2))
+        for i in range (math.ceil(len(pg)/2)):
+            game_data[i] = {
+                "gameDate": (s[i])['GAME_DATE_EST'],
+                "gameStatus": (s[i])['GAME_STATUS_TEXT'],
+                "awayTeamData": {
+                    "logoURL": (pg[2*i])['TEAM_ABBREVIATION'],
+                    "name": teams.find_team_name_by_id((pg[2*i])['TEAM_ID'])['nickname'],
+                    "score": '-'
+                    }, 
+                "homeTeamData": {
+                    "logoURL": (pg[2*i+1])['TEAM_ABBREVIATION'],
+                    "name": teams.find_team_name_by_id((pg[2*i+1])['TEAM_ID'])['nickname'],
+                    "score": '-'
+                    }, 
+                }
     return game_data
 
 # Route for seeing live data
@@ -54,10 +77,6 @@ def get_live_data():
     }
 
 # get past games
-
-from nba_api.stats.static import teams
-
-from nba_api.stats.endpoints import scoreboard as board_of_scores
 def get_past_games():
     today = date.today()
     past_game_data = [{}] * 1
@@ -72,20 +91,19 @@ def get_past_games():
         for i in range (math.ceil(len(pg)/2)):
             game_data[i] = {
                 # "gameStatus": (pg[i])['GAME_STATUS_TEXT'],
-                "gameDate": (pg[2*i+1])['GAME_DATE_EST'],
+                "gameDate": (pg[2*i])['GAME_DATE_EST'],
                 "gameStatus": 'Final',
                 "awayTeamData": {
-                    "logoURL": (pg[2*i+1])['TEAM_ABBREVIATION'],
-                    "name": teams.find_team_name_by_id((pg[2*i+1])['TEAM_ID'])['nickname'],
-                    "score": (pg[2*i+1])['PTS']
-                    }, 
-                "homeTeamData": {
                     "logoURL": (pg[2*i])['TEAM_ABBREVIATION'],
                     "name": teams.find_team_name_by_id((pg[2*i])['TEAM_ID'])['nickname'],
                     "score": (pg[2*i])['PTS']
                     }, 
+                "homeTeamData": {
+                    "logoURL": (pg[2*i+1])['TEAM_ABBREVIATION'],
+                    "name": teams.find_team_name_by_id((pg[2*i+1])['TEAM_ID'])['nickname'],
+                    "score": (pg[2*i+1])['PTS']
+                    }, 
                 }
-            print("hello world", game_data[i])
         past_game_data[d] = {
             'game_day': get_date(num_offset),
             'offset': num_offset,
@@ -120,17 +138,16 @@ def get_future_matchups():
                 "gameDate": (s[i])['GAME_DATE_EST'],
                 "gameStatus": (s[i])['GAME_STATUS_TEXT'],
                 "awayTeamData": {
-                    "logoURL": (pg[2*i+1])['TEAM_ABBREVIATION'],
-                    "name": teams.find_team_name_by_id((pg[2*i+1])['TEAM_ID'])['nickname'],
-                    "score": '-'
-                    }, 
-                "homeTeamData": {
                     "logoURL": (pg[2*i])['TEAM_ABBREVIATION'],
                     "name": teams.find_team_name_by_id((pg[2*i])['TEAM_ID'])['nickname'],
                     "score": '-'
                     }, 
+                "homeTeamData": {
+                    "logoURL": (pg[2*i+1])['TEAM_ABBREVIATION'],
+                    "name": teams.find_team_name_by_id((pg[2*i+1])['TEAM_ID'])['nickname'],
+                    "score": '-'
+                    }, 
                 }
-            print("hello world", game_data[i])
         future_game_data[d] = {
             'game_day': get_date(num_offset),
             'offset': num_offset,
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { React, useState, useEffect } from 'react'
 import LiveData from './live_data.js';
 import PastGames from './past_games.js';
 import FutureMatchups from './future_matchups.js';
+import nba_logo from "./assets/nba-logo.png";
 import './App.css';
 
 function App() {
@@ -24,13 +25,19 @@ function App() {
 
   return (
     <div className="App">
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,800&family=Quicksand&display=swap" rel="stylesheet"></link>
-      {/* <header className="App-header"> </header> */}
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&family=Poppins:ital,wght@1,600;1,800&family=Quicksand&display=swap" rel="stylesheet"></link>
+      <div class="header">
+        <div class="headerTop">
+          <img class = "nbaLogo" alt="nba-logo" src={nba_logo} />
+          <div class="headerText">NBA SCOREBOARD</div>
+        </div>
+        <hr class="headerLine"/>
+      </div>
       <div className="games">
         <div className="dropdown">
           <label for="gameType">Choose Game Day:</label>
           <select className="gameType" id="gameType" value={game_type} onChange={handleChange}>
-            <option value="live">Today</option>
+            <option value="live">Live</option>
             <option value="past">Yesterday</option>
             <option value="future">Tomorrow</option>
           </select>
@@ -38,6 +45,7 @@ function App() {
         {game_type==='live' && <LiveData />}
         {game_type==='past' && <PastGames />}
         {game_type==='future' && <FutureMatchups />}
+        <a href="#" class="top">^</a>
       </div>
     </div>
   );
